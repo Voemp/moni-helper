826552bec758b69737eaa0cbb005c261b22a5f25
Yuyuko9961 <mcfangkuaijun@outlook.com>
2025-03-02 16:34:46 +0800

    🍵 build: Added serialport module in the project

--- src/electron/main/index.ts
+++ src/electron/main/index.ts
@@ -1,5 +1,7 @@
 import { app, BrowserWindow, ipcMain, nativeImage } from 'electron'
 import path from 'node:path'
+import { SerialPort } from 'serialport'
+// import { DelimiterParser } from '@serialport/parser-delimiter'
 
 const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL
 
@@ -12,6 +14,16 @@ const iconPath = VITE_DEV_SERVER_URL
   : path.join(app.getAppPath(), './dist/electron/assets/app_icon.png')
 const appIcon = nativeImage.createFromPath(iconPath)
 
+export interface PortsInfo{
+  path: string
+  manufacturer: string
+  serialNumber: string
+  pnpId: string
+  locationId: string
+  friendlyName: string
+  vendorId: string
+  productId: string
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -25,6 +37,7 @@ const createWindow = () => {
   })
 
   win.loadURL(indexHtmlPath);
+  ipcMain.handle('portScan', getPortsInfo)
 
   win.once('ready-to-show', () => {
     win.show()
@@ -34,8 +47,6 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 
-  ipcMain.on('ping', () => console.log('pong'))
-
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
@@ -44,3 +55,32 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+
+async function getPortsInfo() {
+  try {
+    const ports: PortsInfo[] = await SerialPort.list()
+    return ports
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// getPortsInfo()
+// const sp = new SerialPort({ path: 'COM5', baudRate: 115200 })
+// const parser = sp.pipe(new DelimiterParser({ delimiter: '\n' }))
+
+
+// parser.on('data', chunk => {
+//   console.log(chunk.toString()); // 打印收到的数据
+// });
+
+// 以 paused mode 监听收到的数据，需要主动读取数据
+// sp.on('readable', () => {
+//     console.log(sp.read()); // 使用read方法读取数据，可以指定读取字节数
+// });
+
+// 以 flowing mode 监听收到的数据
+// sp.on('data', (data) => {
+//     console.log(data);
+// });
--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import { PortsInfo } from '../electron/main/index'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const ping = (): void => window.ipcRenderer.send('ping')
+  const [portsInfo, setPortsInfo] = useState<PortsInfo[]>([{
+    path: '',
+    manufacturer: '',
+    serialNumber: '',
+    pnpId: '',
+    locationId: '',
+    friendlyName: '',
+    vendorId: '',
+    productId: ''
+  }]);
+  const getPortsInfo = async () => {
+    const portsInfo: PortsInfo[] = await window.ipcRenderer.invoke('portScan')
+    setPortsInfo(portsInfo)
+  }
 
   return (
     <>
@@ -22,12 +36,20 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)} className="button">
           count is {count}
         </button>
-        <button onClick={ping} className="button">
-          ping
+        <button onClick={getPortsInfo} className="button">
+          test
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div>
+          <h1>Port Information</h1>
+          <p><strong>Path:</strong> {portsInfo[0].path}</p>
+          <p><strong>Manufacturer:</strong> {portsInfo[0].manufacturer}</p>
+          <p><strong>Serial Number:</strong> {portsInfo[0].serialNumber}</p>
+          <p><strong>PNP ID:</strong> {portsInfo[0].pnpId}</p>
+          <p><strong>Location ID:</strong> {portsInfo[0].locationId}</p>
+          <p><strong>Friendly Name:</strong> {portsInfo[0].friendlyName}</p>
+          <p><strong>Vendor ID:</strong> {portsInfo[0].vendorId}</p>
+          <p><strong>Product ID:</strong> {portsInfo[0].productId}</p>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
