8045c81e25e9182ae18dc083ffb73dd1d1f1c80d
Yuyuko9961 <mcfangkuaijun@outlook.com>
2025-03-04 13:41:42 +0800

    🍵 feat: Added basic functions of the backend

--- src/electron/main/index.ts
+++ src/electron/main/index.ts
@@ -1,7 +1,8 @@
 import { app, BrowserWindow, ipcMain, nativeImage } from 'electron'
-import path from 'node:path'
 import { SerialPort } from 'serialport'
-// import { DelimiterParser } from '@serialport/parser-delimiter'
+import { DelimiterParser } from '@serialport/parser-delimiter'
+import * as fs from 'node:fs'
+import path from 'node:path'
 
 const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL
 
@@ -14,7 +15,7 @@ const iconPath = VITE_DEV_SERVER_URL
   : path.join(app.getAppPath(), './dist/electron/assets/app_icon.png')
 const appIcon = nativeImage.createFromPath(iconPath)
 
-export interface PortsInfo{
+export interface PortsInfo {
   path: string
   manufacturer: string
   serialNumber: string
@@ -25,8 +26,99 @@ export interface PortsInfo{
   productId: string
 }
 
+export interface DeviceData {
+  data1: number[]
+  data2: number[]
+  data3: number[]
+  data4: number[]
+}
+
+export enum ResponseCode {
+  PortOpened,
+  PortClosed,
+  PortScanFailed,
+  PortOpenFailed,
+  PortCloseFailed,
+  SaveFileFailed,
+  SaveFileFinished,
+  SaveConfirmation,
+  CacheAlmostFulled,
+  CacheAlreadyFulled,
+  DeviceDisconnected
+}
+
+export class PortData {
+  private dataCache: DeviceData
+  private maxSize: number
+  private sign: boolean
+
+  constructor() {
+    this.dataCache = { data1: [], data2: [], data3: [], data4: [] }
+    this.maxSize = 2000
+    this.sign = true
+  }
+
+  // 初始化数据缓存
+  public init() {
+    this.dataCache = { data1: [], data2: [], data3: [], data4: [] }
+    this.maxSize = 2000
+    this.sign = true
+  }
+
+  // 添加新的数据，保证长度不超过1000000
+  public add(data: number[]): number {
+    const dataLength = this.getLength()
+    if (dataLength >= this.maxSize) {
+      // 数据量超过maxSize,弹出警告
+      this.sign = true
+      return 2
+    } else if ((dataLength >= this.maxSize - 1000) && (this.sign)) {
+      // 数据量即将超过maxSize,停止接受数据
+      this.sign = false
+      return 1
+    }
+    // 添加新数据到数组末尾
+    this.dataCache.data1.push(data[0]);
+    this.dataCache.data2.push(data[1]);
+    this.dataCache.data3.push(data[2]);
+    this.dataCache.data4.push(data[3]);
+    return 0
+  }
+  // 获取当前数组的最后500条数据,不足500条在前方补0
+  public getPromiseData() {
+    const dataToRenderer: DeviceData = { data1: [], data2: [], data3: [], data4: [] }
+    const lenOfSlice = this.getLength() >= 500 ? 500 : this.getLength()
+    dataToRenderer.data1 = this.dataCache.data1.slice(-lenOfSlice)
+    dataToRenderer.data2 = this.dataCache.data2.slice(-lenOfSlice)
+    dataToRenderer.data3 = this.dataCache.data3.slice(-lenOfSlice)
+    dataToRenderer.data4 = this.dataCache.data4.slice(-lenOfSlice)
+    if (lenOfSlice < 500) {
+      dataToRenderer.data1 = Array(500 - lenOfSlice).fill(0).concat(dataToRenderer.data1)
+      dataToRenderer.data2 = Array(500 - lenOfSlice).fill(0).concat(dataToRenderer.data2)
+      dataToRenderer.data3 = Array(500 - lenOfSlice).fill(0).concat(dataToRenderer.data3)
+      dataToRenderer.data4 = Array(500 - lenOfSlice).fill(0).concat(dataToRenderer.data4)
+    }
+    return Promise.resolve(dataToRenderer)
+  }
+  // 获取当前缓存数据的长度
+  public getLength(): number {
+    return this.dataCache.data1.length;
+  }
+  // 获取当前缓存的全部数据
+  public getAllData(): DeviceData {
+    return this.dataCache
+  }
+}
+
+const portData = new PortData
+let isReading = false
+let portsInfo: PortsInfo[]
+let timerId: NodeJS.Timeout
+let sp: SerialPort | null
+let win: BrowserWindow
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     title: 'Moni Helper',
     icon: appIcon,
     frame: true,
@@ -37,7 +129,11 @@ const createWindow = () => {
   })
 
   win.loadURL(indexHtmlPath);
+  ipcMain.on('openPort', (_, deviceName) => startRead(deviceName))
+  ipcMain.on('closePort', stopRead)
+  ipcMain.on('saveFile', saveToCSV)
   ipcMain.handle('portScan', getPortsInfo)
+  ipcMain.handle('readData', getData)
 
   win.once('ready-to-show', () => {
     win.show()
@@ -56,31 +152,182 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
-
+// 获取串口信息
 async function getPortsInfo() {
   try {
-    const ports: PortsInfo[] = await SerialPort.list()
-    return ports
+    portsInfo = await SerialPort.list()
+    if (portsInfo.length == 0) {
+      portsInfo = [{
+        path: '',
+        manufacturer: '',
+        serialNumber: '',
+        pnpId: '',
+        locationId: '',
+        friendlyName: '',
+        vendorId: '',
+        productId: ''
+      }]
+    }
+    return portsInfo
   } catch (error) {
     console.log(error)
+    win.webContents.send('responseMessage', ResponseCode.PortScanFailed)
   }
 }
 
-// getPortsInfo()
-// const sp = new SerialPort({ path: 'COM5', baudRate: 115200 })
-// const parser = sp.pipe(new DelimiterParser({ delimiter: '\n' }))
+// 从缓存中读取数据
+async function getData() {
+  const dataToRenderer: DeviceData = await portData.getPromiseData()
+  return dataToRenderer
+}
 
+// 开启串口读取数据
+function startRead(deviceName: string) {
+  if (isReading || sp) {
+    console.log('alrOpened')
+    return
+  }
+  const portPath = getPath(deviceName)
+  if (portPath.length == 0) {
+    win.webContents.send('responseMessage', ResponseCode.PortScanFailed)
+    return
+  }
 
-// parser.on('data', chunk => {
-//   console.log(chunk.toString()); // 打印收到的数据
-// });
+  sp = new SerialPort({ path: portPath, baudRate: 115200, autoOpen: false })
+  sp.open((err) => {
+    if (err) {
+      win.webContents.send('responseMessage', ResponseCode.PortOpenFailed)
+      return
+    }
 
-// 以 paused mode 监听收到的数据，需要主动读取数据
-// sp.on('readable', () => {
-//     console.log(sp.read()); // 使用read方法读取数据，可以指定读取字节数
-// });
+    // 设备连接情况的心跳检测
+    connectDectect(portPath)
 
-// 以 flowing mode 监听收到的数据
-// sp.on('data', (data) => {
-//     console.log(data);
-// });
+    isReading = true;
+    const parser = sp?.pipe(new DelimiterParser({ delimiter: '\n' }))
+    parser?.on('data', chunk => {
+      const callback = portData.add(convertStringToArray(chunk.toString()))
+      if (callback == 1) {
+        win.webContents.send('responseMessage', ResponseCode.CacheAlmostFulled)
+        console.log("要满了!!!!!!!!!!!!")
+      } else if (callback == 2) {
+        win.webContents.send('responseMessage', ResponseCode.CacheAlreadyFulled)
+        console.log("满了!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        stopRead()
+        saveToCSV()
+      }
+    })
+  })
+}
+
+// 停止串口读取数据
+function stopRead() {
+  if (sp) {
+    console.log('closeNOW!!!')
+    isReading = false
+    sp.removeAllListeners()
+    sp.close((err) => {
+      if (err) {
+        win.webContents.send('responseMessage', ResponseCode.PortCloseFailed)
+      }
+    });
+
+    // 清除通道与连接检测器
+    sp = null
+    clearInterval(timerId)
+  }
+}
+
+function convertStringToArray(input: string): number[] {
+  return input.split(',').map(item => Number(item));
+}
+
+function getPath(deviceName: string) {
+  for (const pInfo of portsInfo) {
+    if (pInfo.serialNumber == deviceName)
+      return pInfo.path
+  }
+  return ''
+}
+
+async function saveToCSV(): Promise<void> {
+  win.webContents.send('responseMessage', ResponseCode.SaveConfirmation)
+  console.log(portData.getLength())
+  const ifsave = await confirmSave()
+  console.log(portData.getLength())
+  if (!ifsave) {
+    return
+  } else {
+    // 停止继续读取
+    stopRead()
+    return new Promise<void>((resolve, reject) => {
+      // 创建文件的写入流
+      const filePath = path.join(app.getAppPath(), './saveData.csv')
+      const writeStream = fs.createWriteStream(filePath)
+      // 获取当前的缓存数据
+      const dataCache = portData.getAllData()
+
+      // 写入CSV的标题行
+      writeStream.write('data1,data2,data3,data4\n')
+
+      // 异步逐行写入数据
+      for (let i = 0; i < portData.getLength(); i++) {
+        const row = [
+          dataCache.data1[i],
+          dataCache.data2[i],
+          dataCache.data3[i],
+          dataCache.data4[i]
+        ];
+        writeStream.write(`${row.join(',')}\n`)
+      }
+
+      // 完成写入后，关闭流
+      writeStream.end()
+
+      // 监听文件流完成事件
+      writeStream.on('finish', () => {
+        resolve()
+      })
+
+      // 监听文件流错误事件
+      writeStream.on('error', () => {
+        reject()
+      })
+
+    }).then(() => afterSave(true))
+      .catch(() => afterSave(false))
+  }
+}
+
+// 保存之后的处理
+function afterSave(sign: boolean) {
+  if (sign) {
+    win.webContents.send('responseMessage', ResponseCode.SaveFileFinished)
+    // 清除数据缓存
+    portData.init()
+  } else {
+    win.webContents.send('responseMessage', ResponseCode.SaveFileFailed)
+  }
+}
+
+// 确认是否保存,保存成功后会自动清除缓存
+function confirmSave(): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    ipcMain.once('confirmSave', (_, sign) => resolve(sign))
+  })
+}
+
+function connectDectect(portPath: string) {
+  timerId = setInterval(async () => {
+    const psInfo = await SerialPort.list()
+    const sign = psInfo.some(pInfo => pInfo.path === portPath)
+    if (!sign) {
+      win.webContents.send('responseMessage', ResponseCode.DeviceDisconnected)
+      console.log('???????????????')
+      stopRead()
+      saveToCSV()
+    } else {
+      console.log('嘻嘻')
+    }
+  }, 1000)
+}
--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import { PortsInfo } from '../electron/main/index'
+import { PortsInfo, DeviceData } from '../electron/main/index'
 import './App.css'
 
 function App() {
@@ -16,10 +16,37 @@ function App() {
     vendorId: '',
     productId: ''
   }]);
+  const [deviceData, setDeviceData] = useState<DeviceData>({ data1: [], data2: [], data3: [], data4: [] })
+  const [btAvailable, setAvailable] = useState<boolean>(false)
   const getPortsInfo = async () => {
     const portsInfo: PortsInfo[] = await window.ipcRenderer.invoke('portScan')
     setPortsInfo(portsInfo)
   }
+  const openPort = () => {
+    window.ipcRenderer.send('openPort', 'FX2348N')
+  }
+  const readData = async () => {
+    const data: DeviceData = await window.ipcRenderer.invoke('readData')
+    setDeviceData(data)
+  }
+  const closePort = () => {
+    window.ipcRenderer.send('closePort')
+  }
+  const saveFile = () => {
+    window.ipcRenderer.send('saveFile')
+  }
+  const confirmSave = (sign: boolean) => {
+    window.ipcRenderer.send('confirmSave', sign)
+    setAvailable(false)
+  }
+  // useEffect(() => {
+  //   console.log('useEffect is running');
+  //   window.ipcRenderer.on('responseMessage', (_, message) => {
+  //     if (message == ResponseCode.SaveConfirmation) {
+  //       setAvailable(true)
+  //     }
+  //   })
+  // },[])
 
   return (
     <>
@@ -37,8 +64,36 @@ function App() {
           count is {count}
         </button>
         <button onClick={getPortsInfo} className="button">
-          test
+          portScan
         </button>
+        <button onClick={openPort} className="button">
+          openPort
+        </button>
+        <button onClick={closePort} className="button">
+          closePort
+        </button>
+        <button onClick={readData} className="button">
+          readData
+        </button>
+        <button onClick={saveFile} className="button">
+          saveFile
+        </button>
+        <button onClick={() => confirmSave(true)} disabled={btAvailable} className="button">
+          confirmTrue
+        </button>
+        <button onClick={() => confirmSave(false)} disabled={btAvailable} className="button">
+          confirmFalse
+        </button>
+      </div>
+      <div>
+        <div>
+          <h1>Numbers</h1>
+          <p><strong>data.1:</strong> {deviceData.data1[0]} {deviceData.data1[249]} {deviceData.data1[499]}</p>
+          <p><strong>data.2:</strong> {deviceData.data2[0]} {deviceData.data2[249]} {deviceData.data2[499]}</p>
+          <p><strong>data.3:</strong> {deviceData.data3[0]} {deviceData.data3[249]} {deviceData.data3[499]}</p>
+          <p><strong>data.4:</strong> {deviceData.data4[0]} {deviceData.data4[249]} {deviceData.data4[499]}</p>
+          <p><strong>length:</strong> {deviceData.data1.length}</p>
+        </div>
         <div>
           <h1>Port Information</h1>
           <p><strong>Path:</strong> {portsInfo[0].path}</p>
@@ -50,7 +105,9 @@ function App() {
           <p><strong>Vendor ID:</strong> {portsInfo[0].vendorId}</p>
           <p><strong>Product ID:</strong> {portsInfo[0].productId}</p>
         </div>
+
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
