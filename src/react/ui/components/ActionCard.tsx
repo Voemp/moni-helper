73ef852b79372a255e0a0ae484bf689b898cb40f
Voemp <Hoshinakiyoru@163.com>
2025-03-06 00:39:39 +0800

    💄 feat: 新增已有数据时，清空数据的选项

--- src/react/ui/components/ActionCard.tsx
+++ src/react/ui/components/ActionCard.tsx
@@ -46,8 +46,21 @@ function ActionCard({
                       type="primary" disabled={!deviceStatus || isMonitoring}>清空数据</Button>
             </Popconfirm>
           }
-          <Button onClick={stopMonitoring} icon={<StopOutlined />} style={{width: "100%", height: "30%"}}
-                  disabled={!deviceStatus || !isMonitoring}>停止监测</Button>
+          {(isMonitoring || !hasGotData) ?
+            <Button onClick={stopMonitoring} icon={<StopOutlined />} style={{width: "100%", height: "30%"}}
+                    disabled={!deviceStatus || !isMonitoring}>停止监测</Button> :
+            <Popconfirm
+              title="确认要清空数据吗？"
+              description="请确保数据已经保存完毕，以防丢失数据。"
+              okText="确认"
+              cancelText="取消"
+              onConfirm={deleteData}
+              placement="bottomRight">
+              <Button danger icon={<DeleteOutlined />} style={{width: "100%", height: "30%"}}
+                      type="primary" disabled={!deviceStatus || isMonitoring}>清空数据</Button>
+            </Popconfirm>
+          }
+
           <Button onClick={saveData} icon={<DownloadOutlined />} style={{width: "100%", height: "30%"}}
                   disabled={isMonitoring || !hasGotData}>保存数据</Button>
         </Flex>
